334708877a61b2d137aee1b959e8f1e17da0329e
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding@users.noreply.github.com>
2026-08-09 09:36:57 -0700

    Update test_output.cpp

--- notebook/tasks/test_output.cpp
+++ notebook/tasks/test_output.cpp
@@ -11,7 +11,7 @@
  * Copyright (C) 2024 Fossil Logic. All rights reserved.
  * -----------------------------------------------------------------------------
  */
-#include <fossil/pizza/framework.h>
+#include <fossil/maip/framework.h>
 #include "fossil/io/framework.h"
 
 // * * * * * * * * * * * * * * * * * * * * * * * *
